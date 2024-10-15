1f4439f2f94d30018038f593145b55e35f12a52a
LookNow <paramas.wae.th@gmail.com>
2024-10-15 21:32:52 +0700

    ♻️ refactor: wrong  text

--- scenes/autoload/game_events.gd
+++ scenes/autoload/game_events.gd
@@ -2,5 +2,5 @@ extends Node
 
 signal experience_vial_colleted(number: float)
 
-func emit_experince_vial_colleted(number: float):
+func emit_experience_vial_colleted(number: float):
 	experience_vial_colleted.emit(number)
--- scenes/game_object/experience_vial/experience_vial.gd
+++ scenes/game_object/experience_vial/experience_vial.gd
@@ -5,5 +5,5 @@ func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
 
 func on_area_entered(other_area: Area2D):
-	GameEvents.emit_experince_vial_colleted(1)
+	GameEvents.emit_experience_vial_colleted(1)
 	queue_free()
