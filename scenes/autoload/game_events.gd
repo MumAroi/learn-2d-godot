de636574c98f8b908e6dda3f7dd6247253ec9578
LookNow <paramas.wae.th@gmail.com>
2024-10-26 12:49:51 +0700

    ♻️ refactor: ability upgrade

--- scenes/autoload/game_events.gd
+++ scenes/autoload/game_events.gd
@@ -1,6 +1,10 @@
 extends Node
 
 signal experience_vial_colleted(number: float)
+signal ability_upgrades_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary)
 
 func emit_experience_vial_colleted(number: float):
 	experience_vial_colleted.emit(number)
+
+func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	ability_upgrades_added.emit(upgrade, current_upgrades)
